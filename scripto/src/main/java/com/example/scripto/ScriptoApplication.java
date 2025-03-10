60eda65044f18cee7101ec78494da222fcbe926f
Debanjan-07 <sdebanjan692@gmail.com>
2025-03-10 23:44:26 +0530

    update

---
+++ scripto/src/main/java/com/example/scripto/ScriptoApplication.java
@@ -0,0 +1,13 @@
+package com.example.scripto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScriptoApplication.class, args);
+	}
+
+}
