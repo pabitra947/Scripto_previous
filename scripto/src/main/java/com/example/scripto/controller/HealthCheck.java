b2f1bc2ff5b6646e31e66ae5f37287f9a8d94633
Pabitra Gupta <pabitragupta947@gmail.com>
2025-03-11 00:01:55 +0530

    Health Check Method added

--- scripto/src/main/java/com/example/scripto/controller/HealthCheck.java
+++ scripto/src/main/java/com/example/scripto/controller/HealthCheck.java
@@ -1,2 +1,13 @@
-package com.example.scripto.controller;public class HealthCheck {
+package com.example.scripto.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+    @GetMapping("/health")
+    public String healthCheck(){
+        return "Everything is good";
+    }
 }
