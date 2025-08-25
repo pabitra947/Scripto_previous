5c847e6916858b023dec05a28beba5981673569e
Pabitra Gupta <pabitragupta947@gmail.com>
2025-08-25 13:38:01 +0530

    update

--- scripto/src/main/java/com/example/scripto/ScriptoApplication.java
+++ scripto/src/main/java/com/example/scripto/ScriptoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+//This is used to run the entire programs
 @SpringBootApplication
 @EnableTransactionManagement
 public class ScriptoApplication {
